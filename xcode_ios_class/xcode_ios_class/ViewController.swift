5de3d0bae1bdd157426e249d3c38d91e4e648f89
Rezo Leshkasheli <rezo_ly@mail.ru>
2023-10-09 18:27:06 +0300

    add hello!

---
+++ xcode_ios_class/xcode_ios_class/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xcode_ios_class
+//
+//  Created by Rezo on 09.10.2023.
+//
+// hello!
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
