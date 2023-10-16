597af1c76c4ec998bf69391c5d8c235bec2a8fcd
Rezo Leshkasheli <rezo_ly@mail.ru>
2023-10-16 19:44:15 +0300

    added url session

--- xcode_ios_class/xcode_ios_class/ViewController.swift
+++ xcode_ios_class/xcode_ios_class/ViewController.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGreen
+        let url: URL = URL(string: "https://api.punkapi.com/v2/beers")!
+        URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
+            guard
+                let data = data, // , - AND
+                let response = response,
+                error == nil
+            else {
+                return
+            }
+            let str = String(data: data, encoding: .utf8)
+            print("Полученные данные: \(str ?? "")")
+        }).resume()
+        
+        
     }
-
-
 }
