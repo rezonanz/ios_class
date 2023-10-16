9da268ac1a1aed4724c2d166d0ea18b417ea44ac
Rezo Leshkasheli <rezo_ly@mail.ru>
2023-10-16 20:04:25 +0300

    added BeerDTO

--- xcode_ios_class/xcode_ios_class/ViewController.swift
+++ xcode_ios_class/xcode_ios_class/ViewController.swift
@@ -15,16 +15,27 @@ class ViewController: UIViewController {
         let url: URL = URL(string: "https://api.punkapi.com/v2/beers")!
         URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
             guard
-                let data = data, // , - AND
-                let response = response,
+                let data = data,
                 error == nil
             else {
                 return
             }
-            let str = String(data: data, encoding: .utf8)
-            print("Полученные данные: \(str ?? "")")
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            //Обработка данных
+            let model = try! decoder.decode([BeerDTO].self, from: data)
+            print(model)
         }).resume()
         
         
     }
+}
+
+
+struct BeerDTO: Decodable {
+    let id: Int
+    let name: String
+    let tagline: String
+    let imageUrl: URL
+    
 }
