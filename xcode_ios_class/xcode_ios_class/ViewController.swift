662a59e2e158cdde6a2255f24e74c83481992cf5
Rezo Leshkasheli <rezo_ly@mail.ru>
2023-10-23 20:40:46 +0300

    commit last

--- xcode_ios_class/xcode_ios_class/ViewController.swift
+++ xcode_ios_class/xcode_ios_class/ViewController.swift
@@ -7,11 +7,48 @@
 // hello!
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return beerData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let beer = beerData[indexPath.row]
+        var cell = UITableViewCell()
+        var configuration = cell.defaultContentConfiguration()
+//        configuration.image
+        configuration.text = beer.name
+        configuration.secondaryText = beer.tagline
+        cell.contentConfiguration = configuration
+        return cell
+    }
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .cyan
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    private var beerData: [BeerDTO] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGreen
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false //не используем авторисайзинг маск (устарело)
+        NSLayoutConstraint.activate([
+            tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+        ])
+        
+        
+        
+        
+        
         let url: URL = URL(string: "https://api.punkapi.com/v2/beers")!
         URLSession.shared.dataTask(with: url, completionHandler: {data, response, error in
             guard
@@ -23,8 +60,10 @@ class ViewController: UIViewController {
             let decoder = JSONDecoder()
             decoder.keyDecodingStrategy = .convertFromSnakeCase
             //Обработка данных
-            let model = try! decoder.decode([BeerDTO].self, from: data)
-            print(model)
+            self.beerData = try! decoder.decode([BeerDTO].self, from: data)
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
         }).resume()
         
         
